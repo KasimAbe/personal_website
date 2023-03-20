7dd74b371de64fd24339e359b5b4e1f31d2d4cb1
Kasim Abe <kasimabe.78@gmail.com>
2023-03-20 00:55:17 -0700

    used for and if statments to check if user input was inside of dictoinary or not

--- java/script.js
+++ java/script.js
@@ -1,1 +1,23 @@
-const courseList = [{code:"ACIT 1620", name: "Web Fundamental Technologies."}, {code:"MATH 1310", name:"Technical math for IT."}, {code:"ACIT 1630",name:"Database Systems."}]
+let courseList = [{code:"1420", name: "Intro to Systems Admin."}, {code:"1310", name:"Technical math for IT."}, {code:"1630",name:"Database Systems."}];
+
+
+let usernumber;
+do {
+  usernumber = prompt("Please enter a 4-digit number:");
+} while (isNaN(usernumber) || usernumber.length !== 4);
+
+
+for (let course of courseList) {
+  if (course.code.includes(usernumber)) {
+    alert("Yes I am taking the course: " + course.code + ' ' + course.name);
+    break;
+  }
+  if (course.code != usernumber) {
+    newcouresname = prompt('What is the course called?: ')
+    const newCourse = { code: usernumber, name: newcouresname };
+    courseList.push(newCourse);
+    alert('Added new course: ' + newCourse.code + ' ' + newCourse.name);
+  }
+  break;
+}
+
